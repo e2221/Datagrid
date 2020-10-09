52e4af14f930613ca9244d72f8db0f04fbfeab08
krall <luke.kral@seznam.cz>
2020-10-09 17:46:37 +0200

    Add column primary

---
+++ src/Column/ColumnPrimary.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+
+namespace e2221\Datagrid\Column;
+
+
+use Nextras\Datagrid\Datagrid;
+
+class ColumnPrimary extends ColumnExtended
+{
+    /**
+     * ColumnId constructor.
+     * @param string $name
+     * @param string|null $label
+     * @param Datagrid $grid
+     */
+    public function __construct(string $name, string $label, Datagrid $grid)
+    {
+        parent::__construct($name, $label, $grid);
+        $this->setHtmlType('text');
+        $this->setHidden();
+        $grid->setRowPrimaryKey($name);
+    }
+}
